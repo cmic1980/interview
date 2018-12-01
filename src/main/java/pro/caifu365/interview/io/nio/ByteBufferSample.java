852a2f06610dade9f9a151c8cd85daaaa1788269
Yu.miao <yu.miao@hinacom.com>
2018-12-01 23:59:16 +0800

    ByteBuffer 例子加入

--- src/main/java/pro/caifu365/interview/io/nio/ByteBufferSample.java
+++ src/main/java/pro/caifu365/interview/io/nio/ByteBufferSample.java
@@ -3,26 +3,33 @@ package pro.caifu365.interview.io.nio;
 import org.apache.commons.io.FilenameUtils;
 
 import java.io.*;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.util.Date;
 
 public class ByteBufferSample {
-    private final static String PIC_FILE_NAME = "IMG_6370.JPG";
 
     public static void main(String[] args) {
-        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024 * 1024);
-        var url = ByteBufferSample.class.getClassLoader().getResource(PIC_FILE_NAME);
-        String fileName = url.getFile();
+        ByteBufferSample sample = new ByteBufferSample();
+        sample.cloneFile();
 
+        sample.writeWithNewFile();
+        sample.writeWithExistingFile();
+    }
+
+    public void cloneFile() {
+        String fileName = "IMG_6370.JPG";
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024 * 1024);
+        var url = ByteBufferSample.class.getClassLoader().getResource(fileName);
+        String fullFileName = url.getFile();
+        String newFileName = this.getNewFileName(fullFileName);
 
         FileChannel ifc = null;
         FileChannel ofc = null;
         try {
-            ifc = new FileInputStream(fileName).getChannel();
-            String ext = FilenameUtils.getExtension(PIC_FILE_NAME);
-            String newFileName = fileName.replace("." + ext, "_New." + ext);
-
+            ifc = new FileInputStream(fullFileName).getChannel();
             ofc = new FileOutputStream(newFileName).getChannel();
 
             int totalSize = 0;
@@ -61,6 +68,47 @@ public class ByteBufferSample {
                 e.printStackTrace();
             }
         }
-        ;
+    }
+
+    public void writeWithNewFile() {
+        // 写文件
+        FileChannel fc = null;
+        try {
+            String filePath = ByteBufferSample.class.getClassLoader().getResource("ByteBuffer_Sample_Data.txt").getPath();
+            String newFileName = this.getNewFileName(filePath);
+
+            FileOutputStream fos = new FileOutputStream(newFileName);
+            fc = fos.getChannel();
+
+            fc.write(ByteBuffer.wrap("New File".getBytes()));  //
+            fc.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeWithExistingFile() {
+        // 写文件
+        FileChannel fc = null;
+        try {
+            String filePath = ByteBufferSample.class.getClassLoader().getResource("ByteBuffer_Sample_Data.txt").getPath();
+            fc = new RandomAccessFile(filePath, "rw").getChannel();
+            fc.position(fc.size()); // Move to the end
+            fc.write(ByteBuffer.wrap("Some more".getBytes()));
+            fc.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getNewFileName(String fileName, String newTag) {
+        String ext = FilenameUtils.getExtension(fileName);
+        String newFileName = fileName.replace("." + ext, newTag + "." + ext);
+        return newFileName;
+    }
+
+    private String getNewFileName(String fileName) {
+        return getNewFileName(fileName, "_New");
     }
 }
